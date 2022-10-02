6fb72725eb78a93897dbeb12fd885f701b44bf44
Astrid <jmdg1023@gmail.com>
2022-10-02 14:17:44 +1100

    lib code

---
+++ index.js
@@ -0,0 +1,9 @@
+const Manager = require("./lib/Manager.js");
+const Engineer = require("./lib/Engineer.js");
+const Intern = require("./lib/Intern.js");
+const inquirer = require("inquirer");
+const path = require("path");
+const fs = require("fs");
+const dist = path.resolve(__dirname, "dist");
+const outputPath = path.join(dist, "team.html");
+const generateTeam = require("./src/template.js")
