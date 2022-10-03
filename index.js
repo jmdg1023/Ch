dc51d2711e55ec2935504e6669aacc80f1bf80c9
Astrid <jmdg1023@gmail.com>
2022-10-03 15:43:17 +1100

    index coding

--- index.js
+++ index.js
@@ -4,6 +4,157 @@ const Intern = require("./lib/Intern.js");
 const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
-const dist = path.resolve(__dirname, "dist");
-const outputPath = path.join(dist, "team.html");
+const OUTPUT_DIR = path.resolve(__dirname, "output");
+const outputPath = path.join(OUTPUT_DIR, "team.html");
 const generateTeam = require("./src/template.js")
+
+teamArray = [];
+
+
+
+function runTest() {
+
+    function createList() {
+        inquirer.prompt([{
+            type: "list",
+            message: "What type of employee would you like to add to your team?",
+            name: "employeeType",
+            choices: ["Manager", "Engineer", "Intern", "No more team members are needed."]
+        }]).then(function (userInput) {
+            switch (userInput.employeeType) {
+                case "Manager":
+                    listManager();
+                    break;
+                case "Engineer":
+                    listEngineer();
+                    break;
+                case "Intern":
+                    listIntern();
+                    break;
+
+                default:
+                    htmlUnicode();
+            }
+        })
+    }
+    // OOP Functions
+
+    function listManager() {
+        inquirer.prompt([
+
+            {
+                type: "input",
+                name: "managerName",
+                message: "What is the manager's name?"
+            },
+
+            {
+                type: "input",
+                name: "managerId",
+                message: "What is the manager's employee ID number?"
+            },
+
+            {
+                type: "input",
+                name: "managerEmail",
+                message: "What is the manager's email address?"
+            },
+
+            {
+                type: "input",
+                name: "managerOfficeNumber",
+                message: "What is the manager's office number?"
+            }
+
+        ]).then(answers => {
+            const manager = new Manager(answers.managerName, answers.managerId, answers.managerEmail, answers.managerOfficeNumber);
+            teamArray.push(manager);
+            createList();
+        });
+
+    }
+
+
+    function listEngineer() {
+        inquirer.prompt([
+
+            {
+                type: "input",
+                name: "engineerName",
+                message: "What is the engineer's name?"
+            },
+
+            {
+                type: "input",
+                name: "engineerId",
+                message: "What is the engineer's employee ID number?"
+            },
+
+            {
+                type: "input",
+                name: "engineerEmail",
+                message: "What is the engineer's email address?"
+            },
+
+            {
+                type: "input",
+                name: "engineerGithub",
+                message: "What is the engineer's GitHub username?"
+            }
+
+        ]).then(answers => {
+            const engineer = new Engineer(answers.engineerName, answers.engineerId, answers.engineerEmail, answers.engineerGithub);
+            teamArray.push(engineer);
+            createList();
+        });
+
+    }
+
+    function listIntern() {
+        inquirer.prompt([
+
+            {
+                type: "input",
+                name: "internName",
+                message: "What is the intern's name?"
+            },
+
+            {
+                type: "input",
+                name: "internId",
+                message: "What is the intern's employee ID number?"
+            },
+
+            {
+                type: "input",
+                name: "internEmail",
+                message: "What is the intern's email address?"
+            },
+
+            {
+                type: "input",
+                name: "internSchool",
+                message: "What school does the intern attend?"
+            }
+
+        ]).then(answers => {
+            const intern = new Intern(answers.internName, answers.internId, answers.internEmail, answers.internSchool);
+            teamArray.push(intern);
+            createList();
+        });
+
+    }
+
+
+    function htmlUnicode() {
+        console.log("Team created!")
+
+        fs.writeFileSync(outputPath, generateTeam(teamArray), "UTF-8")
+
+    }
+
+    createList();
+
+}
+
+runTest();
